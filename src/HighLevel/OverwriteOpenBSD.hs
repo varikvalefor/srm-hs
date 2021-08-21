4d237e6a2ed55d1273af36f36f319ccb7056ff1c
Varik Valefor <varikvalefor@aol.com>
2021-08-21 15:29:15 -0400

    src/HighLevel/OverwriteOpenBSD.hs: Line wrapping improves.

--- src/HighLevel/OverwriteOpenBSD.hs
+++ src/HighLevel/OverwriteOpenBSD.hs
@@ -11,11 +11,11 @@ import qualified Data.Text.Lazy.IO as T;
 -- | @overwritePseudoOpenBSD k@ overwrites all bytes of @k@ with
 -- @\'\\FF\'@, then @\'\\00\'@, and then @\'\\FF\'@.
 --
--- According to the
--- manual page of the original @srm@, this functionality is equivalent
--- to OpenBSD's @rm -P k@.  However, OpenBSD's @rm -P@ functionality has
--- changed, and this functionality no longer resembles the functionality
--- of @rm -P@; as such, the term "pseudo" is used.
+-- According to the manual page of the original @srm@, this
+-- functionality is equivalent to OpenBSD's @rm -P k@.  However,
+-- OpenBSD's @rm -P@ functionality has changed, and this functionality
+-- no longer resembles the functionality of @rm -P@; as such, the term
+-- "pseudo" is used.
 overwritePseudoOpenBSD :: FilePath
                        -- ^ This file path refers to the file which
                        -- should be overwritten.
